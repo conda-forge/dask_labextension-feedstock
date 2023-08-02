d0154b3f35485a6dc45b25b697b5e5fd54d08be5
Nicholas Bollweg <nick.bollweg@gmail.com>
2023-08-02 09:04:24 -0500

    update build script

--- recipe/build_base.sh
+++ recipe/build_base.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -eux
 rm pyproject.toml
-${PYTHON} -m pip install --no-deps -vv --install-option="--skip-npm" .
+${PYTHON} -m pip install --no-deps -vv --no-build-isolation .
